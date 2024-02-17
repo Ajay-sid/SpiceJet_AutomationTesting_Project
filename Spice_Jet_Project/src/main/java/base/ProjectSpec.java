ec96696bcb4aef642635280e056f219405ae22dc
Ajay-sid <ajaysid6299@gmail.com>
2024-02-18 01:42:39 +0530

    Project Creation

---
+++ Spice_Jet_Project/src/main/java/base/ProjectSpec.java
@@ -0,0 +1,120 @@
+package base;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
+import utility.UtilityClass;
+
+public class ProjectSpec extends UtilityClass implements ITestListener{
+	
+	
+	
+//	//@Override
+//	public void onTestFailure(ITestResult result) {
+//	captureScreenshot(result.getMethod().getMethodName()+".jpg");
+//		
+//		
+//	}
+	//Screenshot code for failed testcases
+	public void captureScreenshot(String fileName)  {
+		try {
+			//date
+			LocalDateTime myDateObj = LocalDateTime.now();
+			DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd_MM_yyyy_HHmmss");
+			screenShotFolderName = myDateObj.format(myFormatObj);
+			
+			String formattedDate = myDateObj.format(myFormatObj);
+			System.out.println("After formatting: " + formattedDate);
+			//screenshot code
+			File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			File destination = new File("./ScreenShots/"+screenShotFolderName+"/"+fileName);
+			FileUtils.copyFile(source, destination);
+			System.out.println("Screenshot saved successfully");
+		} catch (Exception e) {
+			System.out.println("Screenshot Failed!");
+			System.out.println(e.getMessage());
+		}
+	}
+	
+
+	@BeforeTest()
+	public void propertiesLoad() throws IOException {
+		
+		 file = new File("src/main/java/property/data.properties");
+		 prop = new Properties();
+		 fis = new FileInputStream(file);
+		 prop.load(fis);
+		 System.out.println("Property Loaded");
+	}
+	
+	@DataProvider(name="getSingleArrayData")
+	public String[] getSingleData() throws IOException {
+	return ReadSingle(excelFile) ;
+	
+	}
+
+	@DataProvider(name="getData")
+	public String[][] getData() throws IOException {
+
+		return  ReadExcel(excelFile);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	@Parameters({"browser","url"})
+	@BeforeMethod()
+	public void launchBrowser(String browser,String url) {
+		 
+		launch(browser, url);
+		System.out.println("browser Launched - before Method");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	@AfterMethod()
+	public void tear(ITestResult result){
+		if(result.getStatus()==ITestResult.FAILURE) {
+			captureScreenshot(result.getTestContext().getName()+"_"+result.getMethod().getMethodName()+".jpg");
+		}
+	close();
+	}
+	
+	
+
+	
+}
---
+++ Spice_Jet_Project/src/main/java/utility/UtilityClass.java
@@ -0,0 +1,268 @@
+package utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//BestBuy
+public class UtilityClass {
+	public WebDriver driver;
+	public File file;
+	public Properties prop;
+	public FileInputStream fis;
+	public String excelFile;
+	public Actions action;
+	public String screenShotFolderName;
+	
+	
+	
+
+	// Launching URL
+	public void launch(String browser, String url) {
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		switch (browser.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver(options);
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;	
+		default:
+			System.err.println("Invalid browser name");
+			break;
+		}
+		
+		driver.manage().window().maximize();
+		try {
+		driver.get(url);
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		
+		}catch(Exception e) {
+			
+		}
+		
+	}
+	
+	public String urlCheck(String link) throws IOException {
+		
+		
+		try {
+			HttpURLConnection huc =(HttpURLConnection)(new URL(link).openConnection());
+			huc.connect();
+			int code =huc.getResponseCode();
+			System.out.println(code);
+			if(code>=400) {
+				return "Link is Broken";
+			}else {
+				return "Link is valid";
+			}
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			return e.getMessage();
+			 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			return e.getMessage();
+		}
+		
+		
+		
+	}
+	
+	public String [] ReadSingle(String excelname) throws IOException {
+			
+		XSSFWorkbook book = new XSSFWorkbook("allData\\"+excelname+".xlsx");
+			XSSFSheet sheet = book.getSheetAt(0);
+
+			int rowCount = sheet.getLastRowNum();
+			String[] data =new String[rowCount];
+			for (int i=1;i<=rowCount;i++) {
+				XSSFRow row = sheet.getRow(i);
+				XSSFCell cell = row.getCell(0);
+				data[i-1]=cell.getStringCellValue();
+				
+			}
+			return data;
+			
+		
+	}
+	
+	
+		
+	// Excel data to array - Appache POI
+	public static String[][] ReadExcel(String excelname) throws IOException {
+
+		XSSFWorkbook book = new XSSFWorkbook("allData\\" + excelname + ".xlsx");
+
+		XSSFSheet sheet = book.getSheetAt(0);
+
+		int rowCount = sheet.getLastRowNum();
+		int colCount = sheet.getRow(0).getLastCellNum();
+//		System.out.println(rowCount);
+//		System.out.println(colCount);
+
+		String[][] data = new String[rowCount][colCount];
+
+		for (int i = 1; i <= rowCount; i++) {
+
+			XSSFRow row = sheet.getRow(i);
+
+			for (int j = 0; j < colCount; j++) {
+
+				XSSFCell cell = row.getCell(j);
+
+				data[i - 1][j] = cell.getStringCellValue();
+			}
+		}
+		book.close();
+		return data;
+
+	}
+	
+	
+	
+	//close active window
+	public void close() {
+
+		driver.close();
+
+	}
+	
+	
+	//close entire browser
+	public void quit() {
+		driver.quit();
+	}
+	
+	//Click an element
+	public void elementClick(WebElement element) {
+		try {
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+		element.click();
+		}catch(WebDriverException e){
+			JavascriptExecutor executor = (JavascriptExecutor)driver;
+			executor.executeScript("arguments[0].click();", element);
+			
+		}
+		
+	}
+	public WebElement getElementFormList(List<WebElement> elements,String str) {
+//		
+		return  elements.stream().filter(m->m.getText().equalsIgnoreCase(str)).findAny().get();
+		
+		
+		
+		//WebElement elem=null;
+//		for(WebElement element :elements) {
+//			if(element.getText().equalsIgnoreCase(str)){
+//				elem=element;
+//				break;
+//			}
+//		}
+//		return elem;
+	}
+	
+	
+	//SendKeys
+	public void sendKeys(WebElement element,String string) {
+		element.sendKeys(string);
+		
+		
+	}
+	public void tabOutofTextField(WebElement element) {
+		element.sendKeys(Keys.TAB);
+		
+		
+	}
+	
+	//------------------------------------switch to--------------------------------//
+	public void switchWindow(String window) {
+		driver.switchTo().window(window);
+	}
+	
+	//Getting page Title
+	public String getPageTitle() {
+        return driver.getTitle();
+    }
+	
+	public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+	
+
+    public void navigateTo(String url) {
+        driver.navigate().to(url);
+    }
+
+    public void refreshPage() {
+        driver.navigate().refresh();
+    }
+
+    public void goBack() {
+        driver.navigate().back();
+    }
+
+    public void goForward() {
+        driver.navigate().forward();
+    }
+    public String getWindowHandle() {
+        return driver.getWindowHandle();
+    }
+    
+    public Set<String> getWindowHandles() {
+        return driver.getWindowHandles();
+    }
+    
+    //-----------------------------Actions--------------------------------//
+    
+    public void actionElementClick(WebElement element) {
+    	action = new Actions(driver);
+    	action.moveToElement(element).click().build().perform();	
+    }
+    
+    public void scrollElement(WebElement element) {
+    	action = new Actions(driver);
+    	action.scrollToElement(element).perform();
+    }
+    public void clickElementInNewWindow(WebElement element) {
+    	action=new Actions(driver);
+		action.moveToElement(element).keyDown(Keys.CONTROL).click().build().perform();
+    }
+    
+}
